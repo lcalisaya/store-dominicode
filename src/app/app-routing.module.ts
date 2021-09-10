d6db96eb19c0f83790e2a9b8d41e82179276bd09
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-10 18:17:03 -0300

    Rutas definidas y una predeterminada para cubrir rutas inexistentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LuciaComponent } from './pages/lucia/lucia.component';
 
-const routes: Routes = [];
+//Las rutas se ejecutan por orden de definición
+const routes: Routes = [
+  {path: 'laruta', component: LuciaComponent},
+  //{path: '**', component:LuciaComponent },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
