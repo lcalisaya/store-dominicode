a0b255ec872080e59acdd9ac00ed52bc9b6fe691
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-10 18:48:03 -0300

    Genero el servicio Products

---
+++ src/app/pages/products/services/products.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  //Servicio disponible para toda la aplicación
+  providedIn: 'root'
+})
+
+export class ProductsService {
+
+  constructor() { }
+}
