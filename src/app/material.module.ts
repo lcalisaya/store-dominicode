f9208e8e9e520fdf47b564cb5e5e22a2b87db94b
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-17 15:05:53 -0300

    Le damos estilos al formulario de checkout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,11 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { MatFormFieldModule} from "@angular/material/form-field";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatRadioModule } from "@angular/material/radio";
-import { MatInputModule } from '@angular/material/input';
+import { MatInputModule } from "@angular/material/input";
+import { MatOptionModule } from "@angular/material/core";
+import { MatSelectModule } from '@angular/material/select';
 //Módulo de Material
 //Decorador que marca el comportamiento de la clase de abajo
 @NgModule({
@@ -16,7 +18,9 @@ import { MatInputModule } from '@angular/material/input';
         MatIconModule,
         MatFormFieldModule,
         MatRadioModule,
-        MatInputModule
+        MatInputModule,
+        MatOptionModule,
+        MatSelectModule
     ]
 })
 
--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -7,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
   model = {
-    name: "lorena",
+    name: "",
     store: "",
     shippingAddress: "",
     city: ""
-  }
+  };
+
+  stores = [
+    {
+      "id": 4,
+      "name": "Luna en Escorpio",
+      "address": "Carlos Casares 8932",
+      "city": "Gregorio de Laferrere",
+      "openingHours": "10:00 - 14:00 y 17:00 - 20:30"
+    },
+    {
+      "id": 5,
+      "name": "La Tierra redonda en el Siglo XXI",
+      "address": "Av. Libertad 1199",
+      "city": "Santiago del Estero",
+      "openingHours": "10:00 - 14:00 y 17:00 - 20:30"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
