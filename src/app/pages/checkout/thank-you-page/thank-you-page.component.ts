5d31fda811a78fdc6384955f221fb80a5d5d93f0
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-18 21:42:13 -0300

    Se da estilos a la página thank-you-page

--- src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
+++ src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-thank-you-page',
-  templateUrl: './thank-you-page.component.html',
+  template: `<div class="container">
+              <h1 class="title">Thank You!</h1>
+              <p class="content">Your order in on the way!</p>
+              <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti amet non molestiae porro odit illum sapiente possimus totam aspernatur? Excepturi impedit, earum dolorem error possimus quia cumque. Id, tenetur autem.
+              </p>
+            </div>`,
   styleUrls: ['./thank-you-page.component.scss']
 })
 export class ThankYouPageComponent implements OnInit {
